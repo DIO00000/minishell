4e5f3236d398424101a74ebb25024eb54cb079c6
DIO <diobrandi@gmail.com>
2024-04-18 14:16:38 +0100

    shell

---
+++ minishell.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbettal <hbettal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 14:46:47 by hbettal           #+#    #+#             */
+/*   Updated: 2024/04/18 07:50:09 by hbettal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_pex
+{
+	int		i;
+	int		end[2];
+	int		input;
+	char	**split_line;
+	int		lines;
+}	t_pex;
+
+char	**ft_split(char *s, char c);
+void	read_command(char **env);
+char	*ft_strjoin(char *s1, char *s2);
+void	fds_closer(int end[]);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strdup(char *s1);
+char	*where_path(char **env);
+size_t	ft_strlen(const char *s);
+void	free_handler(char **str);
+int		ft_strncmp(char *s1, char *s2, size_t n);
+void	ft_here_doc(int end[], int ac, char **av);
+int		count_words(char *str, char c);
+void	special_cases(char **lines, char **env);
+
+#endif
