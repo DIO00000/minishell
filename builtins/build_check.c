5cae0dc89c8f2c60b4b042a8f1ba2b1eb9f95530
DIO <diobrandi@gmail.com>
2024-04-21 17:05:30 +0100

    minishell

--- builtins/build_check.c
+++ builtins/build_check.c
@@ -6,7 +6,7 @@
 /*   By: hbettal <hbettal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 18:47:55 by hbettal           #+#    #+#             */
-/*   Updated: 2024/04/21 10:39:58 by hbettal          ###   ########.fr       */
+/*   Updated: 2024/04/21 17:05:00 by hbettal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int    build_check(char *cmd, t_minishell *mini)
 	int	q;
 
 	q = 0;
-	if (!ft_strncmp(cmd, "cd", 2))
+	if (!ft_strncmp(cmd, "cd ", 3))
 		(cd_build(cmd, mini), q = 1);
 	else if (!ft_strncmp(cmd, "pwd", 3))
 		(pwd_build(cmd), q = 1);
