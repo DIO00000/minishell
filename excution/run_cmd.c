7216781561e096750d45f09cf3e66167ec822665
Hamza Bettal <hbettal@e1r5p14.1337.ma>
2024-06-01 20:09:49 +0100

    fix pipe problem 👍

--- excution/run_cmd.c
+++ excution/run_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hbettal <hbettal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 08:07:55 by hbettal           #+#    #+#             */
-/*   Updated: 2024/06/01 19:39:25 by hbettal          ###   ########.fr       */
+/*   Updated: 2024/06/01 20:08:48 by hbettal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,7 @@ void	middle_commands(t_pex *pex, t_list **data, t_minishell *mini)
 {
 	char	*path;
 	char	**commands;
-	int		i;
 
-	i = 0;
-	commands = mini->final_cmd[pex->i + 1].cmd;
-	while (!commands)
-		i++;
 	if (fork() == 0)
 	{
 		check_fd(mini, pex);
@@ -29,15 +24,16 @@ void	middle_commands(t_pex *pex, t_list **data, t_minishell *mini)
 			exit(mini->exit_status);
 		else
 		{
+			commands = mini->final_cmd[pex->i + 1].cmd;
 			if (dup2(pex->input, 0) < 0)
 				(write (2, "dup2 failed", 11), exit(1));
 			close(pex->input);
-			path = path_check(commands[i], *data, pex->end);
+			path = path_check(commands[0], *data, pex->end);
 			if (!commands || !path)
 				(fds_closer(pex->end), exit(1));
 			fds_closer(pex->end);
 		}
-		execve(path, commands + i, mini->new_env);
+		execve(path, commands, mini->new_env);
 		exit(1);
 	}
 }
@@ -47,11 +43,9 @@ pid_t	last_cmd(t_pex *pex, t_list **data, t_minishell *mini)
 	char	*path;
 	char	**commands;
 	pid_t	id;
-	int		i;
 
-	(1) && (commands = mini->final_cmd[pex->i].cmd, id = fork(), i = 0);
-	while (!commands[i])
-		i++;
+	commands = NULL;
+	id = fork();
 	if (id == 0)
 	{
 		pex->i++;
@@ -60,12 +54,13 @@ pid_t	last_cmd(t_pex *pex, t_list **data, t_minishell *mini)
 			exit(mini->exit_status);
 		else
 		{
-			path = path_check(commands[i], *data, pex->end);
+			commands = mini->final_cmd[pex->i].cmd;
+			path = path_check(commands[0], *data, pex->end);
 			if (!commands || !path)
 				exit(1);
 			fds_closer(pex->end);
 		}
-		execve(path, commands + i, mini->new_env);
+		execve(path, commands, mini->new_env);
 		exit(mini->exit_status);
 	}
 	return (id);
@@ -75,12 +70,7 @@ void	first_cmd(t_list **data, t_pex *pex, t_minishell *mini)
 {
 	char	*path;
 	char	**commands;
-	int		i;
 
-	commands = mini->final_cmd[0].cmd;
-	i = 0;
-	while (!commands[i])
-		i++;
 	if (fork() == 0)
 	{
 		check_fd(mini, pex);
@@ -88,13 +78,14 @@ void	first_cmd(t_list **data, t_pex *pex, t_minishell *mini)
 			exit(mini->exit_status);
 		else
 		{
+			commands = mini->final_cmd[0].cmd;
 			if (!commands)
 				(fds_closer(pex->end), exit(1));
-			path = path_check(commands[i], *data, pex->end);
+			path = path_check(commands[pex->i], *data, pex->end);
 			if (!path)
 				(fds_closer(pex->end), exit(1));
 			fds_closer(pex->end);
 		}
-		(execve(path, commands + i, mini->new_env), exit(1));
+		(execve(path, commands, mini->new_env), exit(1));
 	}
 }
