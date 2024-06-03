14aa2194ffce13de97219873fee890b461d799ca
Hamza Bettal <hbettal@e1r4p14.1337.ma>
2024-06-03 13:01:24 +0100

    heredoc is complete 👍

--- redirect/here_doc.c
+++ redirect/here_doc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oelharbi <oelharbi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hbettal <hbettal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 02:08:56 by hbettal           #+#    #+#             */
-/*   Updated: 2024/06/03 12:27:36 by oelharbi         ###   ########.fr       */
+/*   Updated: 2024/06/03 13:00:29 by hbettal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,11 @@ int	ft_here_doc(char *lim)
 	{
 		str = readline("heredoc> ");
 		if (!ft_strncmp(str, lim, INT_MAX))
+		{
+			close(fd);
+			fd = open("/tmp/secret_f", O_RDWR);
 			return (free(lim), free(str), fd);
+		}
 		write(fd, str, ft_strlen(str));
 		write(fd, "\n", 1);
 		free(str);
